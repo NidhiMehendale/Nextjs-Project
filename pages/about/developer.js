89653975066c62a7b2e59f18ef895bc67c9cd06b
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-09-25 19:56:30 +0530

    Making Nested Routes

---
+++ pages/about/developer.js
@@ -0,0 +1,5 @@
+function DeveloperPage(){
+    return <h1>This is AboutUs developer Page </h1>
+   }
+  
+   export default DeveloperPage;
---
+++ pages/news/something-imp.js
@@ -0,0 +1,7 @@
+//our-domain.com/news/something-imp
+
+function DetailPage(){
+    return <h1>This is News Detail Page</h1>
+   }
+  
+   export default DetailPage;
